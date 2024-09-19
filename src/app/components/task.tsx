6aeeba023e3108039e8a3d8e50fcda4f40399dd2
higorxi <higorgiovane@ucashapp.com>
2024-09-18 21:44:01 -0300

    add component task

--- src/app/components/task.tsx
+++ src/app/components/task.tsx
@@ -1,7 +1,25 @@
-export default function Task(){
-    return(
-        <div>
-            Task
-        </div>
-    )
+"use client";
+import { useState } from "react";
+import styles from '../styles/task.module.scss';
+import { LuTrash } from "react-icons/lu";
+
+export default function Task() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div className={styles.task}>
+      <input 
+        type="checkbox" 
+        className={styles.checkbox} 
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <span className={styles.taskText}>Frase do item</span>
+      <LuTrash className={styles.deleteIcon} />
+    </div>
+  );
 }
