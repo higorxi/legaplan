b13800f17963bf532a60d77cead5dd26e92b21e5
higorxi <higorgiovane@ucashapp.com>
2024-09-18 21:30:36 -0300

    criando todolist container

--- src/app/components/todolist.tsx
+++ src/app/components/todolist.tsx
@@ -1,29 +1,40 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Button from "../ui/button";
 import Task from "./task";
+import styles from '../styles/todolist.module.scss';
 
-export default function TodoList (){
-    const [task, setTasks] = useState(0)
+export default function TodoList() {
+  const [tasks] = useState<number>(3); 
+  const [completedTasks] = useState<number>(1); 
 
-    return(
-        <div>
-            <div>
-            <p>Suas tarefas de hoje</p>
-            <div>
-            <Task/>
-            <Task/>
-            <Task/>
-            </div>
-            <p>Tarefas finalizadas</p>
-            <div>
-            <Task/>
-            </div>
-            </div>
-            <div>
-                <Button
-                />
-            </div>
+  return (
+    <div className={styles.todoListWrapper}>
+      <div className={styles.todoListContainer}>
+        <div className={styles.taskSection}>
+          <p className={styles.title}>Suas tarefas de hoje</p>
+          <div className={styles.taskList}>
+            {[...Array(3)].map((_, index) => (
+              <Task key={index} />
+            ))}
+          </div>
         </div>
-    )
+        <div className={styles.completedTasksSection}>
+          {completedTasks > 0 && (
+            <>
+              <p className={styles.title}>Tarefas finalizadas</p>
+              <div className={styles.completedTasksList}>
+                {[...Array(1)].map((_, index) => (
+                  <Task key={`completed-${index}`} />
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+      <div className={styles.buttonWrapper}>
+        <Button />
+      </div>
+    </div>
+  );
 }
